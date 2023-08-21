1671b368f1478cf0f435b6014fcf500a2c02fd92
Schmelzberry <jelsberry96@gmail.com>
2023-08-21 14:37:22 -0700

    test 3 passed > > Co-authored-by: Richard Barbour <richyb07@gmail.com> Co-authored-by: Similoluwa Oyinkoalde <simi.oyinkolade@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,11 @@ function PlacesGone() {
   this.locations = {};
 }
 
+PlacesGone.prototype.addLocations = function(location) {
+  this.locations = location;
+}
+
+// Locations Logic
 function Locations(name, landmark, season) {
   this.name = name;
   this.landmark = landmark;
